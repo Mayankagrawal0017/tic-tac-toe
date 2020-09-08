5361d533381bf003e577fa5ef0ac7df0e0e88354
Mayank Agrawal <mayankagrawal0017@gmail.com>
2020-09-08 10:10:20 +0530

    remove some uessery print cammand

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 print("hello")
 
-print(3*3)
+print("git"*3)
 
-import random
 import  numpy as np
 A = np.arange(25,512,10)
 A = iter(A)
 print(A)
-print(A.__next__())
-print("And")
+
 for i in A:
     print(i)
